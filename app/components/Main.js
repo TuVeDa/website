acb33d91438ba9521d38b1f2ff4d0f237ee0827c
Genevieve Conty <gmireillec@Genevieves-MBP.attlocal.net>
2017-02-09 22:52:26 -0500

    mobile navbar basic up

--- app/components/Main.js
+++ app/components/Main.js
@@ -15,31 +15,29 @@ var Main = React.createClass({
 	render: function() {
 		return (
 			<div>
-				<div className="container">
-					<nav class="navbar navbar-default">
-						<div class="container-fluid">
-						    {/*Brand and toggle get grouped for better mobile display */}
-						    <div class="navbar-header">
-						      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						        <span class="sr-only">Toggle navigation</span>
-						        <span class="icon-bar"></span>
-						        <span class="icon-bar"></span>
-						        <span class="icon-bar"></span>
-						      </button>
-						      <a class="navbar-brand" href="#">Tuveda</a>
-						    </div>
+				<nav className="navbar navbar-default">
+					<div className="container-fluid">
+					    {/*Brand and toggle get grouped for better mobile display */}
+					    <div className="navbar-header">
+					    	<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						        <span className="sr-only">Toggle navigation</span>
+						        <span className="icon-bar"></span>
+						        <span className="icon-bar"></span>
+						        <span className="icon-bar"></span>
+						    </button>
+					      	<a className="navbar-brand" href="#">TuVeda</a>
+					    </div>
 
-						    {/*Collect the nav links, forms, and other content for toggling */}
-						    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						      <ul className="nav navbar-nav navbar-right">
-							    	<li><a href="#">Our Team</a></li>
-							    	<li><a href="#">Our Projects</a></li>
-							    	<li><a href="#">Contact Us</a></li>
-							    </ul>
-						    </div>
-						</div>
-					</nav>
-				</div>
+					    {/*Collect the nav links, forms, and other content for toggling */}
+					    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					      <ul className="nav navbar-nav navbar-right">
+					        <li><a href="#">Our Team</a></li>
+					        <li><a href="#">Our Projects</a></li>
+					        <li><a href="#">Contact Us</a></li>
+					      </ul>
+					    </div>
+					</div>
+				</nav>
 			</div>
 		);
 	}
