36d6be4f55298ad0c62562eddc8c900418dbe5da
Genevieve Conty <gmireillec@Genevieves-MBP.attlocal.net>
2017-02-09 22:21:58 -0500

    basic scaffolding for mobile navbar

--- app/components/Main.js
+++ app/components/Main.js
@@ -14,19 +14,32 @@ var Main = React.createClass({
 	// Here we render the function
 	render: function() {
 		return (
-			<div className="container">
-				{/*navigation bar*/}
-				<nav className="navbar navbar-default navbar-fixed-top" id="navbar">
-					<div className="container">
-						<a className="navbar-brand " href="#">Tuveda</a>
-						<ul className="nav navbar-nav navbar-right">
-					    	<li><a href="#">Our Team</a></li>
-					    	<li><a href="#">Our Projects</a></li>
-					    	<li><a href="#">Contact Us</a></li>
-					    </ul>
-					</div>
-				</nav>
+			<div>
+				<div className="container">
+					<nav class="navbar navbar-default">
+						<div class="container-fluid">
+						    {/*Brand and toggle get grouped for better mobile display */}
+						    <div class="navbar-header">
+						      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						        <span class="sr-only">Toggle navigation</span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						      </button>
+						      <a class="navbar-brand" href="#">Tuveda</a>
+						    </div>
 
+						    {/*Collect the nav links, forms, and other content for toggling */}
+						    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						      <ul className="nav navbar-nav navbar-right">
+							    	<li><a href="#">Our Team</a></li>
+							    	<li><a href="#">Our Projects</a></li>
+							    	<li><a href="#">Contact Us</a></li>
+							    </ul>
+						    </div>
+						</div>
+					</nav>
+				</div>
 			</div>
 		);
 	}
