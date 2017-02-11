935bb105ab4c94c4515be913bb93d402be92a430
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2017-02-11 14:40:54 -0500

    added appropriate input fields for contact form

--- app/components/children/Contact.js
+++ app/components/children/Contact.js
@@ -8,22 +8,29 @@ var Contact = React.createClass({
 			<div>
 				<form>
 				  <div className="form-group">
-				    <label for="exampleInputEmail1">Email address</label>
-				    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email"/>
+				    <label for="exampleInputEmail1">Name</label>
+				    <input type="email" className="form-control" id="name" placeholder="Your Name"/>
 				  </div>
 				  <div className="form-group">
-				    <label for="exampleInputPassword1">Password</label>
-				    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+				    <label for="exampleInputEmail1">Company Name</label>
+				    <input type="email" className="form-control" id="companyName" placeholder="Your Company Name"/>
 				  </div>
-				  <textarea className="form-control" rows="3" placeholder="Text input"></textarea>
 				  <div className="form-group">
-				    <label for="exampleInputFile">File input</label>
+				    <label for="exampleInputPassword1">Email Address</label>
+				    <input type="password" className="form-control" id="email" placeholder="email@email.com"/>
+				  </div>
+				  <div className="form-group">
+				    <label for="exampleInputPassword1">Tell Us About Your Company</label>
+				    <textarea className="form-control" rows="3" placeholder="For our website, we'd like..."></textarea>
+				  </div>
+				  <div className="form-group">
+				    <label for="exampleInputFile">Attach a File</label>
 				    <input type="file" id="exampleInputFile"/>
-				    <p className="help-block">Example block-level help text here.</p>
+				    <p className="help-block">Add Reference Material</p>
 				  </div>
 				  <div className="checkbox">
 				    <label>
-				      <input type="checkbox"/> Check me out
+				      <input type="checkbox"/> Sign Up for Our Newsletter
 				    </label>
 				  </div>
 				  <button type="submit" className="btn btn-default">Submit</button>
