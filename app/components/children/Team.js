43ab7107fdc20b3d464684503e2fc9db84e29bb6
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2017-02-25 11:43:21 -0500

    landing page pic right size/position, team page mobilized

--- app/components/children/Team.js
+++ app/components/children/Team.js
@@ -8,19 +8,19 @@ var Team = React.createClass({
 			<div id="teamRow">
 				<h1 className="row sectionTitle" >Our Team</h1>
 				<div className="row" id="profRow">
-					<div className="col-md-4 profileBox" id="leftBox">
+					<div className="col-sm-4 profileBox" id="leftBox">
 						<img src="img/blankProf.png" className="profilePic" id="turner"/>
 						<h4>Turner</h4>
 						<p className="subheading">Web Developer</p>
 						<p>Chocolate gummi bears I love danish sweet roll. Wafer wafer toffee I love chocolate bar. Bear claw icing donut cookie tart gummies jujubes I love tart. </p>
 					</div>
-					<div className="col-md-4 profileBox" id="middleBox">
+					<div className="col-sm-4 profileBox" id="middleBox">
 						<img src="img/blankProf.png" className="profilePic" id="veev"/>
 						<h4>Veev</h4>
 						<p className="subheading">Web Developer</p>
 						<p>I love tart gummi bears. Brownie apple pie bear claw icing jujubes croissant candy canes ice cream. I love biscuit chocolate bar caramels. </p>
 					</div>
-					<div className="col-md-4 profileBox" id="rightBox">
+					<div className="col-sm-4 profileBox" id="rightBox">
 						<img src="img/blankProf.png" className="profilePic" id="david"/>
 						<h4>David</h4>
 						<p className="subheading">SEO Specialist</p>
