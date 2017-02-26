0555a7c7899577d83293c34a655d7e0183332bed
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2017-02-26 16:01:56 -0500

    contact form, removed border shadow, sleeker design

--- app/components/children/Contact.js
+++ app/components/children/Contact.js
@@ -70,32 +70,26 @@ var ContactForm = React.createClass ({
 				<h1 className="row sectionTitle">Contact Us</h1>
 				
 				<form className="row" id="formRow" onSubmit={this.props.submitContact}>
-				  <div className="form-group col-md-6">
-				    <label htmlFor="name">Name</label>
-				    <input type="text" className="form-control" id="name" placeholder="Your Name"/>
+				  <div className="form-group col-xs-12 col-sm-6">    
+				    <input type="text" className="form-control" id="name" placeholder="First & Last Name"/>
 				  </div>
-				  <div className="form-group col-md-6">
-				    <label htmlFor="company-name">Company Name</label>
-				    <input type="text" className="form-control" id="company-name" placeholder="Your Company Name"/>
+				  <div className="form-group col-sm-6">
+				    <input type="text" className="form-control" id="company-name" placeholder="Company Name"/>
 				  </div>
-				  <div className="form-group col-md-6">
-				    <label htmlFor="email">Email Address</label>
-				    <input type="email" className="form-control" id="email" placeholder="email@email.com"/>
+				  <div className="form-group col-xs-12">
+				    <input type="email" className="form-control" id="email" placeholder="Email"/>
 				  </div>
-				  <div className="form-group col-md-12">
-				    <label htmlFor="company-description">Tell Us About Your Company</label>
-				    <textarea className="form-control" rows="3" placeholder="For our website, we'd like..." id="company-description"></textarea>
+				  <div className="form-group col-xs-12">
+				    <textarea className="form-control" rows="3" placeholder="Tell Us About Your Company" id="company-description"></textarea>
 				  </div>
-				  <div className="checkbox col-md-12">
+				  <div className="checkbox col-xs-12" id="newsletter">
 				    <label>
-				      <input type="checkbox" id="newsletter" /> Sign Up for Our Newsletter
+				      <input type="checkbox"  /> Sign Up for Our Newsletter
 				    </label>
 				  </div>
-				  <div className="form-group col-md-12">
-
+				  <div className="form-group col-xs-12">
 				    <button type="submit" className="btn btn-default" id="contact-submit">Submit</button>
 				  </div>
-
 				</form>
 			</div>
 		);
